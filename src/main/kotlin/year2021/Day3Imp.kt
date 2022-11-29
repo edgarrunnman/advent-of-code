63a5cad958c2f1daa33a9690ab55fce5f3ba9f02
Edgar Runnman <edgar.runnman@lm.se>
2022-11-29 10:43:51 +0100

    y2021d2s1-done and y2021d2s2-done

---
+++ src/main/kotlin/year2021/Day3Imp.kt
@@ -0,0 +1,14 @@
+package year2021
+import Solution
+import Solution.Companion.getInputData
+
+class Day3Imp: Solution {
+    override val year: Int = 2021 //specify year
+    override val day: Int = 3 //specify day
+    override var puzzleInput: List<String> = getInputData(this.year, this.day)
+
+    //implement first solution
+    override fun solution1(): String = "not rdy"
+    //implement second solution
+    override fun solution2(): String = "not rdy"
+}

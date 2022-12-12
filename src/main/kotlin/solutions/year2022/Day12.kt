9ee23b84ff6c61e84f4b5168d6ade8bdce63816d
Edgar <edgar.runnman@gmail.com>
2022-12-12 22:10:14 +0100

    y2022.d12.p1-done raw y2022.d12.p2-done raw

--- src/main/kotlin/solutions/year2022/Day12.kt
+++ src/main/kotlin/solutions/year2022/Day12.kt
@@ -2,105 +2,131 @@ package solutions.year2022
 
 import DataFetcher
 import solutions.Solution
-import java.util.StringJoiner
+import java.lang.StringBuilder
 
 class Day12(override var dataFetcher: DataFetcher) : Solution {
     override val year: Int = 2022
     override val day: Int = 12
     override lateinit var inputAsList: List<String>
     override lateinit var input: String
-    var register = mutableListOf<String>()
-
     init {
         getInputData()
     }
 
     override fun partOneResult(): String {
         var map = inputAsList.map { line ->
-            line.map { point -> point }
+            line.map { height -> Point(height) }
         }
-        var pathsLst = mutableListOf<Int>()
         var startPosition =
             map.mapIndexed() { x, line -> line.mapIndexed() { y, point -> Pair(point, Position(x, y)) } }
-                .flatMap { it.asIterable() }.first() { it.first == 'S' }
-        map.move(startPosition.second, Path(0, listOf()), pathsLst)
-        return pathsLst.min().toString()
+                .flatMap { it.asIterable() }.first() { it.first.height == 'E' }
+        map.move2(startPosition.second)
+        var endPosition =
+            map.mapIndexed() { x, line -> line.mapIndexed() { y, point -> Pair(point, Position(x, y)) } }
+                .flatMap { it.asIterable() }.first() { it.first.height == 'S' }
 
+        return endPosition.first.steps.toString()
     }
 
     override fun partTwoResult(): String {
-        return "hej"
+        var map = inputAsList.map { line ->
+            line.map { height -> Point(height) }
+        }
+        var startPosition =
+            map.mapIndexed() { x, line -> line.mapIndexed() { y, point -> Pair(point, Position(x, y)) } }
+                .flatMap { it.asIterable() }.first() { it.first.height == 'E' }
+        map.move2(startPosition.second)
+        var endPosition =
+            map.mapIndexed() { x, line -> line.mapIndexed() { y, point -> Pair(point, Position(x, y)) } }
+                .flatMap { it.asIterable() }.filter { it.first.height == 'a' }.map { it.first.steps }.filter { it != null }.sortedBy { it }.first()
+
+        return endPosition.toString()
     }
 
-    private fun getPathChar(list: List<Position>, map: Map): String {
-        return list.map { map[it.x][it.y] }.joinToString("")
-    }
-
-    private fun getPathPoints(list: List<Position>, map: Map): String {
-        return list.map { "${it.x}:${it.y}," }.joinToString("")
-    }
-
-    data class Position(var x: Int, var y: Int)
-
-    class Path(var steps: Int, var visitedPositions: List<Position>)
-
-    private fun Map.move(position: Position, path: Path, list: MutableList<Int>, initSteps: Int = 0) {
-        val visitedPositions = path.visitedPositions + position
-        val legitMoves = getLegitMoves(visitedPositions, this)
-        legitMoves.forEach { pos ->
-//            register.add(getPathPoints(visitedPositions, this))
-//            print(register.distinct().count())
-//            print("-")
-//            print(register.count())
-//            println()
-            println(visitedPositions.count())
-            if (this[pos.x][pos.y] == 'E') {
-                val count = path.visitedPositions.count() + 1
-                if (list.isEmpty()) list.add(count)
-                if (path.steps + 1 < list.min()) list.add(count)
-            } else {
-                this.move(pos, Path(path.steps + 1, visitedPositions), list)
+    private fun getStepsMap(map: Map): String {
+        var string = StringBuilder()
+        map.forEach { line ->
+            line.forEach { point ->
+                string.append(if (point.steps == null) point.height else point.steps)
             }
+            string.appendLine()
+        }
+        return string.toString()
+    }
+
+    private fun Map.move2(pos: Position) {
+        var step = 1
+        var legitMoves = markNeighbours(pos, this, step)
+        while (legitMoves.count() != 0) {
+            step++
+            var newLegitMoves = listOf<Position>()
+            legitMoves.forEach { nextPositon ->
+                newLegitMoves = newLegitMoves + markNeighbours(nextPositon, this, step)
+            }
+            var stepsMap = getStepsMap(this)
+            legitMoves = newLegitMoves
         }
     }
 
-    private fun getLegitMoves(visitedPositions: List<Position>, map: Map): List<Position> {
-        val pos = visitedPositions.last()
-        var height = map[pos.x][pos.y]
-        if (height == 'S') height = 'a'
-        val maxHeight = height + 1
-        val legitPositions = mutableListOf<Position>()
+    private fun markNeighbours(pos: Position, map: Map, steps: Int) : List<Position>{
+        var height = map[pos.x][pos.y].height
+        if (height == 'E') height = 'z'
+        val minHeight = height - 1
+        var legitPositions = mutableListOf<Position>()
         if (pos.x > 0) {
-            var nextHeight = map[pos.x - 1][pos.y]
-            if (nextHeight == 'E') nextHeight = 'z'
-            if (nextHeight <= maxHeight)
-                legitPositions.add(Position(pos.x - 1, pos.y))
+            var point = map[pos.x - 1][pos.y]
+            if (point.steps == null || point.steps!! > steps){
+                var nextHeight = point.height
+                if (nextHeight == 'S') nextHeight = 'a'
+                if (nextHeight >= minHeight){
+                    map[pos.x - 1][pos.y].steps = steps
+                    legitPositions.add(Position(pos.x - 1, pos.y))
+                }
+            }
         }
 
         if (pos.x < map.count() - 1) {
-            var nextHeight = map[pos.x + 1][pos.y]
-            if (nextHeight == 'E') nextHeight = 'z'
-            if (nextHeight <= maxHeight)
-                legitPositions.add(Position(pos.x + 1, pos.y))
+            var point = map[pos.x + 1][pos.y]
+            if (point.steps == null || point.steps!! > steps){
+                var nextHeight = point.height
+                if (nextHeight == 'S') nextHeight = 'a'
+                if (nextHeight >= minHeight){
+                    map[pos.x + 1][pos.y].steps = steps
+                    legitPositions.add(Position(pos.x + 1, pos.y))
+                }
+            }
         }
 
         if (pos.y > 0) {
-            var nextHeight = map[pos.x][pos.y - 1]
-            if (nextHeight == 'E') nextHeight = 'z'
-            if (nextHeight <= maxHeight)
-                legitPositions.add(Position(pos.x, pos.y - 1))
+            var point = map[pos.x][pos.y - 1]
+            if (point.steps == null || point.steps!! > steps){
+                var nextHeight = point.height
+                if (nextHeight == 'S') nextHeight = 'a'
+                if (nextHeight >= minHeight){
+                    map[pos.x][pos.y - 1].steps = steps
+                    legitPositions.add(Position(pos.x, pos.y - 1))
+                }
+            }
         }
 
         if (pos.y < map[pos.x].count() - 1) {
-            var nextHeight = map[pos.x][pos.y + 1]
-            if (nextHeight == 'E') nextHeight = 'z'
-            if (nextHeight <= maxHeight)
-                legitPositions.add(Position(pos.x, pos.y + 1))
+            var point = map[pos.x][pos.y + 1]
+            if (point.steps == null || point.steps!! > steps){
+                var nextHeight = point.height
+                if (nextHeight == 'S') nextHeight = 'a'
+                if (nextHeight >= minHeight){
+                    map[pos.x][pos.y + 1].steps = steps
+                    legitPositions.add(Position(pos.x, pos.y + 1))
+                }
+            }
         }
-        return legitPositions.filter { !visitedPositions.contains(it) }
+        return legitPositions
     }
 }
-
-typealias Map = List<List<Char>>
+data class Position(var x: Int, var y: Int)
+data class Point(val height: Char ){
+    var steps: Int? = null
+}
+typealias Map = List<List<Point>>
 
 

2c8439e454170ca70021cfebd4d4e54f6f794250
Edgar Runnman <edgar.runnman@lm.se>
2022-12-15 15:01:29 +0100

    y2022.d15.p1-works on testdata

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val runner = RunnerImp(2022)
 //    runner.runAll()
-    runner.runByDay(14)
+    runner.runByDay(15)
 }
--- src/main/kotlin/solutions/year2022/Day15.kt
+++ src/main/kotlin/solutions/year2022/Day15.kt
@@ -2,6 +2,7 @@ package solutions.year2022
 
 import DataFetcher
 import solutions.Solution
+import kotlin.math.abs
 
 class Day15(override var dataFetcher: DataFetcher) : Solution {
     override val year: Int = 2022
@@ -14,13 +15,103 @@ class Day15(override var dataFetcher: DataFetcher) : Solution {
     }
 
     override fun partOneResult(): String {
-        var foo = inputAsList
-        return foo.toString()
+        var line = 10
+        var foo = inputAsList.map { it.getSB() }
+        var foo2 = foo.filter { it.inLine(line) }
+        var foo3 = foo2.getAllLineCoverage(line)
+        var foo4 = foo3.distinct().sorted()
+        return (foo4.count()).toString()
     }
 
     override fun partTwoResult(): String {
         var foo = inputAsList
         return foo.toString()
+    }
+
+
+//    private fun SB.printSB(line: Int){
+//        val xS = this.s.x
+//        val xB = this.b.x
+//        val yS = this.s.y
+//        val yB = this.b.y
+//        val size = abs(xB - xS) + abs(yB - yS)
+//
+//        (0 .. size * 2).map { line ->
+//            (0..size * 2).map { point ->
+//
+//            }
+//        }
+//
+//
+//
+//
+//        val maxCov = abs(yS - yB)
+//        val lineCov = maxCov - dy + abs(xS- xB)
+//        val result = mutableListOf<Int>()
+//        (0..lineCov).forEach {
+//            result.add(it + this.s.x)
+//            result.add(0 - it + this.s.x)
+//        }
+//        return result
+//    }
+
+    data class Point(var x: Int, var y: Int)
+    data class SB(var s: Point, var b: Point)
+
+    private fun String.getSB(): SB {
+        val xRegex = "x=-*\\d+".toRegex()
+        val yRegex = "y=-*\\d+".toRegex()
+        val stringSB = this.split(": ")
+        val s = Point(
+            xRegex.find(stringSB[0])!!.value.split("=")[1].toInt(),
+            yRegex.find(stringSB[0])!!.value.split("=")[1].toInt()
+        )
+        val b = Point(
+            xRegex.find(stringSB[1])!!.value.split("=")[1].toInt(),
+            yRegex.find(stringSB[1])!!.value.split("=")[1].toInt()
+        )
+        return SB(s, b)
+    }
+
+    private fun SB.inLine(lineIndex: Int): Boolean {
+        val xS = this.s.x
+        val xB = this.b.x
+        val yS = this.s.y
+        val yB = this.b.y
+        val dx = abs(xB - xS)
+        val dy = abs(yB - yS)
+        var yMax = 0
+        var yMin = 0
+        if (dx < dy) {
+            yMax = yB
+            yMin = yS - dy
+        } else {
+            yMax = yS + dx
+            yMin = yS - dx
+        }
+        return lineIndex in (yMin..yMax)
+    }
+
+    private fun SB.getLineCoverage(lineIndex: Int): List<Int> {
+        val xS = this.s.x
+        val xB = this.b.x
+        val yS = this.s.y
+        val yB = this.b.y
+        val dy = abs(this.s.y - lineIndex)
+        val maxCov = abs(yS - yB)
+        val lineCov = maxCov - dy + abs(xS- xB)
+        val result = mutableListOf<Int>()
+        (0..lineCov).forEach {
+            result.add(it + this.s.x)
+            result.add(0 - it + this.s.x)
+        }
+        return result
+    }
+
+    private fun List<SB>.getAllLineCoverage(lineIndex: Int): List<Int> {
+        val result = mutableListOf<Int>()
+        this.forEach { result.addAll(it.getLineCoverage(lineIndex)) }
+        return result
     }
 }
 

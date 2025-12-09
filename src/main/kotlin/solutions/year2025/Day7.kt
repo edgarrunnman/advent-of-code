7727e5a970b01a3d3f55e4bb498b46271ecc5858
Edgar <edgar.runnman@gmail.com>
2025-12-09 06:57:23 +0100

    2025.7.1

--- src/main/kotlin/solutions/year2025/Day7.kt
+++ src/main/kotlin/solutions/year2025/Day7.kt
@@ -2,9 +2,6 @@ package solutions.year2025
 
 import DataFetcher
 import solutions.Solution
-import java.math.BigInteger
-import kotlin.plus
-import kotlin.times
 
 class Day7(override var dataFetcher: DataFetcher) : Solution {
     override val year: Int = 2025
@@ -17,7 +14,31 @@ class Day7(override var dataFetcher: DataFetcher) : Solution {
     }
 
 
-    override fun partOneResult(): String = TODO()
+    override fun partOneResult(): String {
+        val startIndex = inputAsList.first().indexOf("S")
+        val beams = mutableSetOf(startIndex)
+        val manyfolds = inputAsList.drop(1)
+        var counter = 0
+        manyfolds.forEach { manyfold ->
+            val newBeams = mutableSetOf<Int>()
+            val removedBeams = mutableSetOf<Int>()
+
+            beams.forEach { beam ->
+                if (manyfold.get(beam) == '^') {
+                    counter++
+                    removedBeams.add(beam)
+                    newBeams.add(beam - 1)
+                    newBeams.add(beam + 1)
+                }
+            }
+            removedBeams.forEach { removedBeam ->
+                beams.remove(removedBeam)
+            }
+            beams.addAll(newBeams)
+        }
+        return counter.toString()
+    }
 
     override fun partTwoResult(): String = TODO()
 }
+
